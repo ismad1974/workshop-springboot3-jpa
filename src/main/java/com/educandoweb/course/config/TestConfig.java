1f566c930d58faea751a653d2f3becf962a4b029
Ismael Abreu <ismad1.abreu@gmail.com>
2025-08-09 12:17:44 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,8 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -32,6 +37,21 @@ public class TestConfig implements CommandLineRunner {
 
 		User[] listUsers = { u1, u2, u3, u4, u5, u6, u7, u8, u9, u10 };
 		userRepository.saveAll(Arrays.asList(listUsers));
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u2);
+		Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
+		Order o5 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
+		Order o6 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u4);
+		Order o7 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o8 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u2);
+		Order o9 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
+		Order o10 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u4);
+
+		Order[] listOrders = { o1, o2, o3, o4, o5, o6, o7, o8, o9, o10 };
+		orderRepository.saveAll(Arrays.asList(listOrders));
+
 	}
 
 }
