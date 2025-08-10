3c1e144e856e81cededc508b96751ecbf0004361
Ismael Abreu <ismad1.abreu@gmail.com>
2025-08-10 13:32:09 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -38,16 +39,16 @@ public class TestConfig implements CommandLineRunner {
 		User[] listUsers = { u1, u2, u3, u4, u5, u6, u7, u8, u9, u10 };
 		userRepository.saveAll(Arrays.asList(listUsers));
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u2);
-		Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
-		Order o5 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
-		Order o6 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u4);
-		Order o7 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		Order o8 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u2);
-		Order o9 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
-		Order o10 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u4);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u3);
+		Order o5 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u3);
+		Order o6 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u4);
+		Order o7 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
+		Order o8 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u2);
+		Order o9 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u3);
+		Order o10 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u4);
 
 		Order[] listOrders = { o1, o2, o3, o4, o5, o6, o7, o8, o9, o10 };
 		orderRepository.saveAll(Arrays.asList(listOrders));
