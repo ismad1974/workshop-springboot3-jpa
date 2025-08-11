97ca08a437f58008e26d13e0d94e002600d6c79b
Ismael Abreu <ismad1.abreu@gmail.com>
2025-08-10 21:34:18 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -13,26 +13,31 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String umgUrl;
 
 	@Transient
-	private Set<Product> produts = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 
-	public Category() {
-		super();
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String umgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.umgUrl = umgUrl;
 	}
 
 	public Long getId() {
@@ -51,8 +56,32 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	public Set<Product> getProduts() {
-		return produts;
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getUmgUrl() {
+		return umgUrl;
+	}
+
+	public void setUmgUrl(String umgUrl) {
+		this.umgUrl = umgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -68,7 +97,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
 
